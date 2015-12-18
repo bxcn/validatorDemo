0b2f0e330d9c395e0606d201e351f2839129a148
bxcn <hi_beijing@126.com>
2015-12-18 17:20:48 +0800

    create
    init

---
+++ gulpfile.babel.js
@@ -0,0 +1,70 @@
+// generated on 2015-11-17 using generator-gulp-webapp 1.0.3
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+gulp.task('uglify',  () => {
+  return gulp.src(['app/js/**/*.js'])
+    .pipe(gulp.dest('./dist/js'))
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.js', $.rename({extname: ".min.js"})))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('sass',  () => {
+  return gulp.src(['app/sass/**/*.scss'])
+    .pipe($.if('*.scss', $.sass.sync({
+        outputStyle: 'compact', //输出css格式 nested（默认嵌套）, expanded（展开的）, compact（紧凑的）, compressed（压缩的）
+        precision: 10,
+        includePaths: ['.']
+      }).on('error', $.sass.logError)))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('html', () => {
+  return gulp.src(['app/*.html',  '!app/html/test/**/*.html'])
+    .pipe($.if('*.html', $.ejs({},{ext: '.html'})))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task("json", () => {
+  return gulp.src(['./app/json/**/*.json'])
+    .pipe(gulp.dest('dist/json/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('serve', [ 'uglify',  'html', 'sass', 'json'], () => {
+  browserSync({
+    notify: false,
+    open:true,
+    port: 9000,
+    browser: "google chrome",
+    //reloadDelay: 20000, // 延迟20000毫秒重新加载
+    server: {
+      baseDir: ['dist'],
+      routes: {
+        '/bower_components': '../../../../bower_components'
+      }
+    }
+  });
+
+  gulp.watch([
+    'app/**/*.html'
+  ]).on('change', reload);
+
+  gulp.watch(['app/**/*.html', 'app/js/**/*.js'], [ 'html', 'uglify']);
+  gulp.watch(['app/sass/*.scss'], [ 'sass']);
+});
+
+
+
+
+
+
+
